d00fcfd6e3c43accbfc775dd769621442845e55a
Thaileng Thol <64580685+thailengthol@users.noreply.github.com>
2021-06-18 11:43:07 +0700

    Create 5-lib.sh

---
+++ library_installation/5-lib.sh
@@ -0,0 +1,20 @@
+set -x #echo on
+
+cd $DIR/zlib-1.2.7
+./configure --prefix=$DIR/grib2
+make
+make install
+
+cd $DIR/libpng-1.2.50
+./configure --prefix=$DIR/grib2
+make
+make install
+
+cd $DIR/jasper-1.900.1
+./configure --prefix=$DIR/grib2
+make
+make install
+
+sudo ln -sf /home/thaileng/wrf-cordex/libraries/grib2/lib/libpng12.so.0 /usr/lib/x86_64-linux-gnu/libpng12.so.0
+
+echo "Done"

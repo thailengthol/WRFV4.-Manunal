596776aaabd318cb1eb2833d0b2c1b31c875bd83
Thaileng Thol <64580685+thailengthol@users.noreply.github.com>
2021-06-18 11:33:23 +0700

    Create getWRF.sh

---
+++ library_installation/getWRF.sh
@@ -0,0 +1,8 @@
+cd ./
+
+git clone https://github.com/wrf-model/WRF
+git clone https://github.com/wrf-model/WPS
+
+wget https://zenodo.org/record/1469651/files/WRFV4.0_CORDEX_v1.3.tar.gz
+
+mv WRFV4.0_CORDEX_v1.3.tar.gz ./WRF

f4ee000983f3782a9ee7334539d082ceb8e6b343
Thaileng Thol <64580685+thailengthol@users.noreply.github.com>
2021-06-18 11:44:00 +0700

    Create 6-lib.sh

---
+++ library_installation/6-lib.sh
@@ -0,0 +1,20 @@
+set -x #echo on
+
+cd $DIR
+mkdir ncl-6.6.2
+cd ncl-6.6.2
+wget https://www.earthsystemgrid.org/dataset/ncl.662_2.nodap/file/ncl_ncarg-6.6.2-Debian9.8_64bit_nodap_gnu630.tar.gz
+tar xzvf ncl_ncarg-6.6.2-Debian9.8_64bit_nodap_gnu630.tar.gz
+
+cd $DIR
+mkdir Gfortran
+cd Gfortran
+wget http://gfortran.meteodat.ch/download/x86_64/releases/gcc-6.3.0.tar.xz
+tar xf gcc-6.3.0.tar.xz
+# require sudo password or do this manualy
+sudo apt install libfontconfig1 libxrender1
+
+#error while loading shared libraries: libpng12.so.0: cannot open shared object file: No such file or directory
+sudo ln -sf $DIR/grib2/lib/libpng12.so.0 /usr/lib/x86_64-linux-gnu/libpng12.so.0
+
+echo "Done"

0ef32e31c77f315ee9b506d4b6af96e426110127
Thaileng Thol <64580685+thailengthol@users.noreply.github.com>
2021-07-05 16:41:21 +0700

    Update getwrf.sh

--- library_installation/getwrf.sh
+++ library_installation/getwrf.sh
@@ -3,6 +3,7 @@ cd ./
 git clone https://github.com/wrf-model/WRF
 git clone https://github.com/wrf-model/WPS
 
+cd ./WRF
 wget https://zenodo.org/record/1469651/files/WRFV4.0_CORDEX_v1.3.tar.gz
 
-mv WRFV4.0_CORDEX_v1.3.tar.gz ./WRF
+# mv WRFV4.0_CORDEX_v1.3.tar.gz ./WRF

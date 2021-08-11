5755fee3789c57a38add7141b871adb16445c836
Thaileng Thol <64580685+thailengthol@users.noreply.github.com>
2021-08-11 15:33:20 +0700

    Update 1lib.sh

--- library_installation/1lib.sh
+++ library_installation/1lib.sh
@@ -1,10 +1,12 @@
-set -x #echo on
+set -x echo
+
 source ~/.bash_aliases
+mkdir -p $DIR
 
 sudo apt-get update
 sudo apt-get install build-essential csh gfortran m4
 apt install make
 apt install g++
-sudo apt install libfontconfig1 libxrender1
+#sudo apt install libfontconfig1 libxrender1
 
 echo "Done"

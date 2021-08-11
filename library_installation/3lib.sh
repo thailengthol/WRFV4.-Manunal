c6f56ec2b415faa924eef1c750a3392e7b3727ea
Thaileng Thol <64580685+thailengthol@users.noreply.github.com>
2021-08-11 15:34:15 +0700

    Update 3lib.sh

--- library_installation/3lib.sh
+++ library_installation/3lib.sh
@@ -5,26 +5,4 @@ cd $DIR/netcdf-4.1.3
 make
 make install
 
-cd $DIR/mpich-3.0.4
-./configure --prefix=$DIR/mpich
-make
-make install
-
-cd $DIR/zlib-1.2.7
-./configure --prefix=$DIR/grib2
-make
-make install
-
-cd $DIR/libpng-1.2.50
-./configure --prefix=$DIR/grib2
-make
-make install
-
-cd $DIR/jasper-1.900.1
-./configure --prefix=$DIR/grib2
-make
-make install
-
-sudo ln -sf /home/thaileng/wrf-cordex/libraries/grib2/lib/libpng12.so.0 /usr/lib/x86_64-linux-gnu/libpng12.so.0
-
 echo "Done"
